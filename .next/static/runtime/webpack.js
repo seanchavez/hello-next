703b4a0be91b6e12d4441c8c1dd6ae100e132bfb
Sean Chavez <realseanchavez@gmail.com>
2018-12-28 20:35:14 -0800

    creat about page and link to it

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "76957b9c4f26e5ca7a48";
+/******/ 	var hotCurrentHash = "656146869e4ba6f322e0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
